104486cb6c4962fe5b559be21d98bb6333ba4bfb
Krishanu Konar <krishh_konar@yahoo.com>
2016-03-25 02:51:52 +0530

    search query added

--- stream_tweets.py
+++ stream_tweets.py
@@ -1,4 +1,4 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 ''' 
 Fetches streaming tweets in real time.
@@ -25,9 +25,11 @@ class MyListener(StreamListener):
 				#See Twitter reference for what fields are included -- https://dev.twitter.com/docs/platform-objects/tweets
 				line1 = "@" + str(j['user']['screen_name']) + " on " + j['created_at'][:-11] + ", language= "+ j["lang"] + ": "
 				line2 = '\n' + j['text']
-				#line3 = '\n    Retweets: %d, Favourites: %d' % (j['retweet_count'],j['favorite_count'])
 				text = line1 + line2
 				print text + "\n\n"
+
+				# writes the streamed data to tweets.json
+				# if you want to save the tweets only, replace "data" with "text" in the line below
 				f.write(data + "\n\n")
 				
 
@@ -41,38 +43,40 @@ class MyListener(StreamListener):
 
 
 def main():
-
-	# Authentication for using twitter data
-	auth = OAuthHandler(t.CONSUMER_KEY,t.CONSUMER_SECRET)
-	auth.set_access_token(t.ACCESS_TOKEN,t.ACCESS_TOKEN_SECRET)
-
 	#creating a REST_API instance
-	api = API(auth)
+	api, auth = authenticate()
 
 	try:
 		#creating Streaming_API instance
 		stream = Stream(auth, MyListener())
-
 		selfInfo(api)  ## Show self information
-		#search query
-		print "\n Tweets: \n\n"
 		
-
-		columns = os.popen('stty size', 'r').read().split()[1]
-		stream.filter(track = ['arsenal'])
+		#search query
+		query = raw_input('Search query: ')
+		print "\nTweets:"
+		print "-------\n\n"
+		stream.filter(track = [query])
 
 	except KeyboardInterrupt:
 		print '\n Stopped Fetching Tweets.'
 		exit()
 
+def authenticate():
+	''' Authentication for using twitter data '''
+	auth = OAuthHandler(t.CONSUMER_KEY, t.CONSUMER_SECRET)
+	auth.set_access_token(t.ACCESS_TOKEN,t.ACCESS_TOKEN_SECRET)
+	api = API(auth)
+	return api, auth
 
 def selfInfo(api):
 	'''print information about authenticated user '''
 	user = api.me()
 	columns = os.popen('stty size', 'r').read().split()[1]
-	print "{:^{}}".format("User-Information",columns)
-	print '{:^{}}'.format("================",columns)
-	print "\n"+"{:^{}}".format(user.name,columns) + '\n' + "{:^{}}".format("@"+user.screen_name,columns) + '\n  ' + "{:^{}}".format(user.description,columns) + "\n\n"
+	print '{:^{}}'.format("======================",columns)
+	print "{:^{}}".format("|| User-Information ||",columns)
+	print '{:^{}}'.format("======================",columns)
+	print "\n"+"{:^{}}".format(user.name,columns) + '\n' + "{:^{}}".format("@"+user.screen_name,columns) + '\n  ' + "{:^{}}".format(user.description,columns) 
+	print "=" * int(columns) +"\n\n"
 	return
 
 if __name__ == '__main__':
